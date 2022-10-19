6eb974b3851b6d763f2f4d38434ad7a2397ff8bd
Ornella Facciola <102764364+ornefacciola@users.noreply.github.com>
2022-10-19 18:06:16 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -5,12 +5,25 @@ import Button from '@mui/material/Button';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import './App.css';
+import db from './firebase-config';
+import { collection, getDocs } from "firebase/firestore";
 
 const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
   const [todos, setTodos] = useState([]) 
   const todoNameRef = useRef()
+  
+  useEffect(() => {
+    const getData = async() => {
+      const datas = await getDocs(collection(db, 'users'))
+      datas.forEach((documents) => {
+        console.log(documents.data());
+      });
+    }
+
+    getData()
+  }, []);
 
   useEffect(() => { 
     const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
@@ -31,7 +44,7 @@ function App() {
     const name = todoNameRef.current.value
     if (name === "") return 
     setTodos(prevTodos => {  
-      return [...prevTodos, { id: uuidv4(), name: name, complete: false, editItem: false }]
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false }]
     })
     todoNameRef.current.value = null 
   }
@@ -47,12 +60,6 @@ function App() {
   }
 
 
-  function confirmationButton() {
-    const answer= window.confirm("Are you sure you want to delete the selected items?");
-    if (answer) {
-        handleClearTodos()
-      }
-  }
 
   return (
     <>
@@ -61,11 +68,11 @@ function App() {
           <div className='row justify-content-center'>
           <h1 className='todo row justify-content-center' style={{color: 'white' }}>To-Do List</h1>
           <div>
-            <input className='inp col-9' style={{color: 'white'}} ref={todoNameRef} type="text" placeholder="what is your to do?" /> 
+            <input className='inp col-9' style={{color: 'white'}} ref={todoNameRef} type="text" placeholder="What is your to do?" /> 
           </div>
           <div className="row justify-content-center row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2">
             <Button variant="contained" className='col-9 col-sm-9 col-md-6  col-lg-6' color="success" onClick={handleAddTodo}>Add todo</Button>
-            <Button variant="outlined" className='col-9 col-sm-9 col-md-3 col-lg-3' color="error"component="label" onClick={confirmationButton}>Clear todos</Button>
+            <Button variant="outlined" className='col-9 col-sm-9 col-md-3 col-lg-3' color="error"component="label" onClick={handleClearTodos}>Clear todos</Button>
           </div>
           <TodoList todos={todos} toggleTodo={toggleTodo} removeTodo={removeTodo} />
           <div style={{color: 'rgb(151, 151, 151)'}}>{todos.filter(todo => !todo.complete).length} left to do</div> 
